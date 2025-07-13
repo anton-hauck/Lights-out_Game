f0086d465f97c26812e77f0a2b2dbc2e33400b83
Anton Hauck <antonhauck@gmail.com>
2025-07-13 14:43:02 +0200

    Looks

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import light_manager
+import questionary
 
 def main():
     light_manager_object = light_manager.light_manager(4,4)
     table = light_manager_object.return_table()
+    #print(light_manager_object.lights[2].neighbours)
+    #light_manager_object.lights[2].switched()
+    #print(light_manager_object.lights[2].neighbours)
     for row in table:
-        print(row)
-    print(light_manager_object.lights[5].neighbours)
-    light_manager_object.lights[5].switched()
-    print(light_manager_object.lights[5].neighbours)
-    for row in table:
-        print(row)
+        row_print = []
+        for light in row:
+            if light.on:
+                row_print.append("X")
+            else:
+                row_print.append("O")
+        print(row_print)
+
+    while True:
+        #hier weitermachen mit questionary
 
 
 

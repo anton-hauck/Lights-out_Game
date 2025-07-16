b68892177a7bb5ebbcc9110eb14d4907262eecfb
Anton Hauck <antonhauck@gmail.com>
2025-07-17 00:11:50 +0200

    main() upgrade

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
+from operator import truediv
+
 import light_manager
 import questionary
 
-def main():
-    light_manager_object = light_manager.light_manager(4,4)
-    table = light_manager_object.return_table()
-    #print(light_manager_object.lights[2].neighbours)
-    #light_manager_object.lights[2].switched()
-    #print(light_manager_object.lights[2].neighbours)
+def display_game(table):
     for row in table:
         row_print = []
         for light in row:
@@ -16,8 +13,23 @@ def main():
                 row_print.append("O")
         print(row_print)
 
-    while True:
-        #hier weitermachen mit questionary
+def validate_dimension(text):
+    if not text.isnumeric():
+        return "Please enter a number."
+    if int(text) <= 0:
+        return "Please enter a number greater than 0."
+    return True
+
+
+def main():
+    height = int(questionary.text("Please enter the height", validate=validate_dimension).ask())
+    width = int(questionary.text("Please enter the width", validate=validate_dimension).ask())
+    light_manager_object = light_manager.light_manager(height, width)
+    table = light_manager_object.return_table()
+    #print(light_manager_object.lights[2].neighbours)
+    #light_manager_object.lights[2].switched()
+    #print(light_manager_object.lights[2].neighbours)
+    display_game(table)
 
 
 
